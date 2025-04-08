9656373927462962391d5066c9df966920f706cc
ajaysth <157970942+ajaysth@users.noreply.github.com>
2025-04-08 05:27:47 +0000

    compared

---
+++ 1_basics/comparisons.js
@@ -0,0 +1,24 @@
+// console.log(3 > 5);
+// console.log(3 < 5);
+// console.log(3 == 5);
+// console.log(3 <= 5);
+// console.log(3 >= 5);
+// console.log(3 != 5);
+
+// console.log("3" > 2);
+// console.log("3" < 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+console.log("3" === 4);
+console.log("ad" === "ad");
+console.log(true === 1);
+
+console.log(3 === 4);
+console.log(9 === 9);
+
+
+
