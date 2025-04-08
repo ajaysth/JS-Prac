bb9f482b626fcc3d2cfbe49034495604a24c7c37
ajaysth <157970942+ajaysth@users.noreply.github.com>
2025-04-08 05:04:00 +0000

    dataconvs

--- 1_basics/dataconversion.js
+++ 1_basics/dataconversion.js
@@ -1,10 +1,40 @@
-let age = 22
-console.log(age)
+let age = 22;
+// console.log(age);
 
-let conAge = String(age)
-console.log(conAge)
-console.log(typeof conAge)
+let conAge = String(age);
+// console.log(conAge);
+// console.log(typeof conAge);
 
-let variable = "22aa"
-let res = Number(variable)
-console.log(res)
+let variable = "22aa";
+let res = Number(variable);
+// console.log(res);
+
+
+/////////operations/////
+let val = 4;
+let negVal = -val;
+// console.log(negVal);
+
+// console.log(3+3);
+// console.log(8-3);
+// console.log(3*3);
+// console.log(81/3);
+// console.log(4**3);
+// console.log(2%3);
+
+
+let str1 = "Ajay";
+let str2 =  " Shrestha";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("2" + 3 + 3)
+// console.log(2 + 3 + "3");
+
+
+console.log(+true);
+console.log(+"");
+
+
+
+
