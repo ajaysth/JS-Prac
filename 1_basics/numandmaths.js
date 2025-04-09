89762a116e8f20191d5ba7485d740a7533bb5a16
ajaysth <157970942+ajaysth@users.noreply.github.com>
2025-04-09 11:06:39 +0000

    nums a

---
+++ 1_basics/numandmaths.js
@@ -0,0 +1,42 @@
+const score = 200
+// console.log(score)
+
+const balance = new Number(300)
+// console.log(balance);
+// console.log(balance.toString().length);
+
+const bal = 400.33443
+// console.log(bal);
+// console.log(bal.toFixed(3));
+// console.log(bal.toPrecision(4));
+
+const zeros = 100000000
+// console.log(zeros.toLocaleString());
+
+//********************maths */
+// console.log(Math.PI);
+// console.log(Math.abs(-3));
+// console.log((Math.round(3.6)));
+// console.log(Math.ceil(3.1));
+// console.log(Math.floor());
+
+// console.log(Math.random());
+
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor((Math.random()*10)+1));
+
+
+const max = 20
+const min = 10
+
+console.log(Math.random() * (max-min+1) + min);
+
+
+
+
+
+
+
+
+
+
