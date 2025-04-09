473534be77f6ebd11c33d0e48de09275def8c2c4
ajaysth <157970942+ajaysth@users.noreply.github.com>
2025-04-09 11:40:38 +0000

    date and time

---
+++ 1_basics/datetime.js
@@ -0,0 +1,28 @@
+let myDate = new Date()
+console.log(myDate.getMonth());
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+
+let newdate = new Date(2021, 0,21,5,52)
+// console.log(newdate);
+// console.log(newdate.toDateString());
+// console.log(newdate.toLocaleDateString());
+// console.log(newdate.toLocaleString());
+
+let date = new Date("01-23-2022")
+// console.log(newdate);
+// console.log(newdate.toDateString());
+// console.log(newdate.toLocaleString());
+// console.log(newdate.getTime());
+
+
+let myTimestamp = Date.now()
+// console.log(Math.floor(Date.now()/1000));
+
+// console.log(myTimestamp);
+
+
