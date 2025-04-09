6c04287897f5622f8827f653f64e7f68c7eee40f
ajaysth <157970942+ajaysth@users.noreply.github.com>
2025-04-09 07:13:58 +0000

    some changes

---
+++ 1_basics/string.js
@@ -0,0 +1,7 @@
+const name = new  String('ajay-shrestha')
+// console.log(`My name is ${name}`)
+// console.log(name[3])
+// console.log(name.__proto__);
+// console.log(name.charAt(3))
+
+const res = name.subString()
