5337a5c58c671b4bd6648b32a8dbcaa5e16c1f06
ajaysth <157970942+ajaysth@users.noreply.github.com>
2025-04-15 11:41:15 +0000

    function with array and objects and rest operators

--- 1_basics/functions.js
+++ 1_basics/functions.js
@@ -23,4 +23,34 @@ function UserLoggedIn(user){
     }
 }
 
-console.log(UserLoggedIn())
+// console.log(UserLoggedIn())
+
+
+//Rest operator: used to collect multiple input in parameters (...)
+
+function Sum(...num){
+    return num
+}
+
+// console.log(Sum(2,3,4,5))
+
+
+const User={
+    username : "Ajay",
+    price : 222
+}
+
+function objHandle(anyObject){
+    console.log(`user name is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+// objHandle(User)
+
+
+const myArr = [2,3,22,43,1,0]
+
+function retArr(val){
+    return val[3]
+}
+
+console.log(retArr(myArr));
