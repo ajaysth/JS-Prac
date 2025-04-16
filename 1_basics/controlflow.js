833911b118c54598e825dbc7994e9a3d359069e3
ajaysth <157970942+ajaysth@users.noreply.github.com>
2025-04-16 14:26:23 +0000

    conditional done

---
+++ 1_basics/controlflow.js
@@ -0,0 +1,22 @@
+// const marks = 99;
+
+// if (marks > 80) {
+//     console.log("A grade"); 
+// }else{
+//     console.log("Failed");
+    
+// }
+
+
+const marks = 34;
+
+if (marks >= 80) {
+    console.log("A grade"); 
+}else if(marks > 60 && marks < 80){
+    console.log("B grade");   
+}else if(marks > 40 && marks <=60){
+    console.log("Just passed");
+}else{
+    console.log("Failed Try again"); 
+}
+
---
+++ 1_basics/switch.js
@@ -0,0 +1,54 @@
+
+// let num = 1;
+
+
+// switch (num) {
+//     case 1:
+//         console.log("Its 1");
+//         break;
+//     case 2:
+//         console.log("Its 2");
+//         break;
+//     case 3:
+//         console.log("Its 3");
+//         break;
+        
+//     default:
+//         console.log("Nothing try more");
+        
+//         break;
+// }
+
+
+// let useremail =[]
+// if(useremail.length === 0){
+//     console.log("the array is empty");
+    
+// }
+
+// let example = {};
+// if(Object.keys(example).length === 0){
+//     console.log("The object is empty");
+    
+// }
+
+
+///nullish operator
+
+let val;
+// val = 12 ?? 3
+// val = 12 ?? null
+// val = null ?? null
+// val = null ?? 2
+val = undefined ?? 3
+// console.log(val);
+
+
+
+///ternary operator
+
+// condition ? true : false
+
+let value = 2
+value <=20 ? console.log("Its less than 20") : console.log("No its not");
+
