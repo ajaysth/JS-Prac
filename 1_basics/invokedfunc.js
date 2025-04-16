6efc7973eeb050093259fd44429203085c040d04
ajaysth <157970942+ajaysth@users.noreply.github.com>
2025-04-16 12:15:17 +0000

    iife

--- 1_basics/invokedfunc.js
+++ 1_basics/invokedfunc.js
@@ -1,3 +1,17 @@
-function PrintMsg(){
+(function PrintMsg(){
     console.log("Hello World");
-}
+}) ();
+
+
+(function PrintAnother(){
+    console.log("I am another");
+}) ();
+
+(() => {
+    console.log("I am arrow function");
+}) ();
+
+
+((name) => {
+    console.log(`I am ${name}`);
+}) ("Ajay");
